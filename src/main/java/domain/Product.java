02e18a59cdfb9da7215b051da88aff78a27f0acf
Jack Baucke <bauja773@student.otago.ac.nz>
2018-07-16 17:06:51 +1200

    Added product domain class. References #1.

---
+++ src/main/java/domain/Product.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author bauja773
+ */
+public class Product {
+    private int productID;
+    private String name;
+    private String description;
+    private String category;
+    private BigDecimal listPrice;
+    private Integer quantity;
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public BigDecimal getListPrice() {
+        return listPrice;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setListPrice(BigDecimal listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }    
+
+    @Override
+    public String toString() {
+        return "Product{" + "productID=" + productID + ", name=" + name + '}';
+    }
+    
+}
