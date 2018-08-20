4bf28d8061f18dc1e48aa880e6d52dd6cdf9df08
Jack Baucke <bauja773@student.otago.ac.nz>
2018-08-20 17:23:26 +1200

    Added junit dependency.

---
+++ src/test/java/NewEmptyJUnitTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author bauja773
+ */
+public class NewEmptyJUnitTest {
+    
+    
+    
+    public NewEmptyJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
